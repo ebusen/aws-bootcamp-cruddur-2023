bf79eb0417ccbaa030a4a9ae580943a414a4046e
Ebubekir SEN <senebubekir@gmail.com>
2023-03-20 01:39:25 +0000

    Reinstalled aws cli, postgres and fetched gitpod ip again

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -8,6 +8,6 @@ INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
     (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
-    'This was imported as seed data!',
+    'This was imported as seed data!!!',
     current_timestamp + interval '10 day'
   )
